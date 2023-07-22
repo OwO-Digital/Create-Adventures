2c6db0cf6e4a6a65fcde7eca0ff00696a79b6721
Aproxia-dev <apro@r4ilax.eu>
2023-07-22 19:06:03 +0200

    Unify Azalea Flowers (Solves #9)

--- kubejs/server_scripts/tweaks.js
+++ kubejs/server_scripts/tweaks.js
@@ -1,5 +1,32 @@
 ServerEvents.recipes(event => {
   event.shapeless(Item.of('minecraft:pointed_dripstone', 4), ['minecraft:dripstone_block'])
+
+  event.shapeless(Item.of('twigs:azalea_flowers', 3), ['ecologics:azalea_flower'])
+  event.replaceInput({ input: 'immersive_weathering:azalea_flowers' },
+    'immersive_weathering:azalea_flowers',
+    'twigs:azalea_flowers'
+  )
+  event.remove({output: 'immersive_weathering:azalea_flower_pile'})
+  event.replaceInput({ output: 'immersive_weathering:flower_crown' },
+    'twigs:azalea_flowers',
+    'ecologics:azalea_flower'
+  )
+
+  const azalea_wood_blocks = [
+    "azalea_log",
+    "azalea_wood",
+    "azalea_planks",
+    "azalea_slab",
+    "azalea_stairs",
+    "azalea_fence",
+    "azalea_fence_gate",
+    "azalea_door",
+    "azalea_trapdoor"
+  ]
+
+  azalea_wood_blocks.forEach(block => {
+    event.shapeless('ecologics:flowering_' + block, ['ecologics:' + block, 'twigs:azalea_flowers'])
+  })
 })
 
 ServerEvents.tags('item', event => {
